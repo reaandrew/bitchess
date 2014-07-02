c3ad3fb628a86bd98b64bb61eeec3c1775690123
REAANDREW <email@andrewrea.co.uk>
2014-07-02 23:13:21 +0100

    Got more of a feel for what I need to do now so I can start again with tests

--- test/chess.c
+++ test/chess.c
@@ -3,10 +3,11 @@
 typedef unsigned long int board;
 
 board createPositionMask(char position[2]){
-    int column = (position[0] - 97) + 1;
-    int row = position[1] - '0'; 
-    board newPosition = 0 | 0x01;
-    return newPosition << ((column*row)-1);
+    int fileIndex = (position[0] - 97);
+    int rankIndex = position[1] - '0'; 
+    int squareIndex = 8*rankIndex + fileIndex;
+    board newPosition = 1 << squareIndex;
+    return newPosition;
 }
 
 board createMoveMask(char from[2], char to[2]){
@@ -20,7 +21,7 @@ board move(board *b, char from[2], char to[2]){
     return *b ^ moveMask;
 }
 
-void printbits(board num){
+void printzeebits(board num){
     int i;
     for(i= (sizeof(unsigned long int)*8)-1;i>= 0; i--){
         char c = (num &(1LL<<i))? '1' : '0';
@@ -39,13 +40,15 @@ void printboard(board *b){
         squares[i] = c;
     }
 
-    for(i = 0; i < 8; i++){
-        for(j = 0; j < 8; j++){
-            putchar(squares[(j*8)+i]); 
+    for(i = 63; i >= 0; i-=8){
+        for(j = 7; j >= 0; j--){
+            putchar(squares[i-j]);
+            //printf("%d",i-j);
             putchar(' ');
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 void initialize_pawns(board *b){
@@ -66,10 +69,21 @@ void initialize_whitepawns(board *b){
 
 int main(){
     board black_pawns = 0;
-    board white_pawns = 0;
-    initialize_pawns(&black_pawns);
-    initialize_whitepawns(&white_pawns);
-    board result = black_pawns | white_pawns;
-    result = move(&result, "h2", "h3");
+    /*
+       board white_pawns = 0;
+       initialize_pawns(&black_pawns);
+       initialize_whitepawns(&white_pawns);
+       board result = black_pawns | white_pawns;
+       result = move(&result, "a2","a3");
+       */
+    board result = black_pawns | 0x0101010101010101;
+    printboard(&result);
+    result = black_pawns | 0x00000000000000FF;
+    printboard(&result);
+    result = black_pawns | 0xFF00000000000000;
+    printboard(&result);
+    result = black_pawns | 0x8040201008040201;
+    printboard(&result);
+    result = black_pawns | 0x55AA55AA55AA55AA;
     printboard(&result);
 }
